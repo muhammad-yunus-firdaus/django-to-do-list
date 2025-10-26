0a6b4bbd59254440e05062ef6f97526a6fa09554
Muhammad Yunus Firdaus <yunusfirdauss123@gmail.com>
2025-10-26 10:30:03 +0700

    Pindah dari PostgreSQL ke SQLite untuk hosting di PythonAnywhere

--- manajemen_tugas/settings.py
+++ manajemen_tugas/settings.py
@@ -1,6 +1,7 @@
 """
 Django settings for manajemen_tugas project.
 """
+import dj_database_url
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -79,10 +80,10 @@ WSGI_APPLICATION = 'manajemen_tugas.wsgi.application'
 
 # Database configuration
 DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        conn_max_age=600,
+    )
 }
 
 #DATABASES = {
